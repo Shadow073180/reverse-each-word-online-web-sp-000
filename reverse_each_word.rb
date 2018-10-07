6261d072f2bdfe1d7804686a04fb07f14f32b023
David masterson <david73180@gmail.com>
2018-10-07 01:14:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,5 @@
-def reverse_each_word(sentence1)
-  array =[]
-  sentence1.split
-    sentence1.map do |x|
-      x.reverse
-      array << x
-    end
-  return array
+def reverse_each_word(sentence)
+  sentence1.collect {|x| x + !}
+
 end
+
