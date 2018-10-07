9b8757e3f427451b70d61f758109d3be54c3ff7c
David masterson <david73180@gmail.com>
2018-10-07 01:24:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,13 @@ def reverse_each_word(string)
   new_string.each {|x| new_array << x.reverse!}
   new_array.join(" ")
 end
+
+
+def reverse_each_word(string)
+  newnew_string = string.split
+  newnew_string.collect {|x| x.reverse!}
+
+
+
+
+end
